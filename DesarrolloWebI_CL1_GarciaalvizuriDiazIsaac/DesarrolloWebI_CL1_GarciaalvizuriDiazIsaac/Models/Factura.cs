1d232de9b7bae6ca0e85d8cb1169d867d99b68d3
Isaac <isaac769@hotmail.com>
2023-07-17 02:24:21 -0500

    Creado modelo "Factura"

---
+++ DesarrolloWebI_CL1_GarciaalvizuriDiazIsaac/DesarrolloWebI_CL1_GarciaalvizuriDiazIsaac/Models/Factura.cs
@@ -0,0 +1,12 @@
+﻿namespace DesarrolloWebI_CL1_GarciaalvizuriDiazIsaac.Models
+{
+    public class Factura
+    {
+        public int idFactura { get; set; }
+        public DateTime fechaEmision { get; set; }
+        public string? nombreProducto { get; set; }
+        public decimal preciounitario { get; set; } 
+        public decimal cantidad { get; set; }   
+        public decimal monto { get; set; }  
+    }
+}

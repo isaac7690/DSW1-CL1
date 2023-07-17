82fe54ddedab7f9cf83d2d078a8422dab1e015ed
Isaac <isaac769@hotmail.com>
2023-07-17 03:17:00 -0500

    Agregada dependencia, librerías e indicada cadena de conexión a usar

--- DesarrolloWebI_CL1_GarciaalvizuriDiazIsaac/DesarrolloWebI_CL1_GarciaalvizuriDiazIsaac/Controllers/FacturasController.cs
+++ DesarrolloWebI_CL1_GarciaalvizuriDiazIsaac/DesarrolloWebI_CL1_GarciaalvizuriDiazIsaac/Controllers/FacturasController.cs
@@ -1,9 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration; //para obtener la info de la cadena de conexión del appsettings
+using Microsoft.Data.SqlClient;
+using DesarrolloWebI_CL1_GarciaalvizuriDiazIsaac.Models;
 
 namespace DesarrolloWebI_CL1_GarciaalvizuriDiazIsaac.Controllers
 {
     public class FacturasController : Controller
     {
+        private readonly IConfiguration _config;
+        private string cadena;
+
+        //para indicar que conexión a BD de las disponibles en el appsettings usaremos
+        public FacturasController(IConfiguration _config)
+        {
+            this._config = _config;
+            this.cadena = _config["ConnectionStrings:connection"];
+        }
+
         public IActionResult Index()
         {
             return View();

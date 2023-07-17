afc8fff299e448157185446cc2420f2cd9b0ec18
Isaac <isaac769@hotmail.com>
2023-07-17 02:30:07 -0500

    Creado FacturasController

---
+++ DesarrolloWebI_CL1_GarciaalvizuriDiazIsaac/DesarrolloWebI_CL1_GarciaalvizuriDiazIsaac/Controllers/FacturasController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DesarrolloWebI_CL1_GarciaalvizuriDiazIsaac.Controllers
+{
+    public class FacturasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

fa704017f1df14e244993e425e830f13d1ddd478
Isaac <isaac769@hotmail.com>
2023-07-17 04:37:03 -0500

    Añadido a cada atributo del modelo Factura etiquetas Display Name

--- DesarrolloWebI_CL1_GarciaalvizuriDiazIsaac/DesarrolloWebI_CL1_GarciaalvizuriDiazIsaac/Models/Factura.cs
+++ DesarrolloWebI_CL1_GarciaalvizuriDiazIsaac/DesarrolloWebI_CL1_GarciaalvizuriDiazIsaac/Models/Factura.cs
@@ -1,12 +1,20 @@
-﻿namespace DesarrolloWebI_CL1_GarciaalvizuriDiazIsaac.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DesarrolloWebI_CL1_GarciaalvizuriDiazIsaac.Models
 {
     public class Factura
     {
+        [Display(Name = "ID")]
         public int idFactura { get; set; }
+        [Display(Name = "Fecha de Emisión")]
         public DateTime fechaEmision { get; set; }
+        [Display(Name = "Producto")]
         public string? nombreProducto { get; set; }
-        public decimal preciounitario { get; set; } 
-        public decimal cantidad { get; set; }   
+        [Display(Name = "Precio Unitario")]
+        public decimal preciounitario { get; set; }
+        [Display(Name = "Cantidad")]
+        public decimal cantidad { get; set; }
+        [Display(Name = "Monto")]
         public decimal monto { get; set; }  
     }
 }

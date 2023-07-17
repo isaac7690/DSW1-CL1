e27d75ea46342000bcdc0a4e3c683ea85d8bbd46
Isaac <isaac769@hotmail.com>
2023-07-17 08:41:07 -0500

    Creada clase Cliente / Creadas etiquetas display

---
+++ DesarrolloWebI_CL1_GarciaalvizuriDiazIsaac/DesarrolloWebI_CL1_GarciaalvizuriDiazIsaac/Models/Cliente.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DesarrolloWebI_CL1_GarciaalvizuriDiazIsaac.Models
+{
+    public class Cliente
+    {
+        [Display(Name = "ID Cliente")]
+        public int idCliente { get; set; }
+        [Display(Name = "Cliente")]
+        public string? razonSocial { get; set; }
+        [Display(Name = "Dirección")]
+        public string? dirección { get; set; }
+        [Display(Name = "Teléfono")]
+        public string? telefono { get; set; }
+        [Display(Name = "ID Categoría")]
+        public int idCategoriaCliente { get; set; }
+        [Display(Name = "Categoría")]
+        public string? nombreCategoria { get; set; }
+
+    }
+}
